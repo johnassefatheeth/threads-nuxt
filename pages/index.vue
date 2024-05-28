5ee582f48328f47c9f10dbdfa09f086e6ccf620e
John Assefa <johnassefaforbussines@gmail.com>
2024-05-28 04:55:37 -0700

    makeing the posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,42 @@
 <template>
-    <MainLayout/>
+    <MainLayout>
+        <div id="IndexPage" class="w-full overflow-auto">
+            <div class="mx-auto max-w-[500px] overflow-hidden">
+                <div id="posts" class="px-4 max-w-[600px] mx-auto">
+                    <div class="text-white" v-if="isPosts" v-for="post in posts" :key="post">
+                        {{ post }}
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </MainLayout>
     
 </template>
 
 <script setup>
 import MainLayout from '~/layout/MainLayout.vue';
+import { useUserStore } from '~/stores/user';
 
+const userStore=useUserStore()
+// const user=useSupabaseUser()
+
+let posts=ref([])
+let isPosts=ref(true)
+let isLoading=ref(false)
+
+
+onBeforeMount(()=>{
+    posts.value=[
+        {
+            name:'john assefa',
+            image:'https://placeholder.co/100',
+            text:'title',
+            pic:'https://placeholder.co/500'
+        }
+    ]
+})
 </script>
