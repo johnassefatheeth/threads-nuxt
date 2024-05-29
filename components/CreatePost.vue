048278d8447db75a8dc43611d55fac2e258a4482
John Assefa <johnassefaforbussines@gmail.com>
2024-05-29 05:57:32 -0700

    made and connected  create post overlay

--- components/CreatePost.vue
+++ components/CreatePost.vue
@@ -1,6 +1,15 @@
 <template>
-    <div id="CreatePost" class="fixed z-50 bottom-0 h-full overflow-hidden">
+    <div id="CreatePost" class="fixed z-50 bottom-0 w-full h-full overflow-hidden">
+        <div class="bg-black h-full w-full text-white overflow-auto">
+            <div class="flex items-center justify-start py-4 max-w-[500px] mx-auto border-b border-b-gray-700">
+                <button @click="userStore.isMenuOverlay=false; clearData()" class="rounded-full px-2">
+                    <Icon name="mdi:close" size="25"/>
 
+                </button>
+                <div class="text-[16px] font-semibold">New Thread</div>
+            </div>
+
+        </div>
     </div>
 </template>
 
