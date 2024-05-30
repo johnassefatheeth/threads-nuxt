7238668546aeb650c3d8e4d3c5c910265eb54c3a
John Assefa <johnassefaforbussines@gmail.com>
2024-05-30 01:00:27 -0700

    began authentication

---
+++ pages/auth.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import { watchEffect } from 'vue';
+
+    // const user=useSupabaseUser()
+    // const client=useSupabaseClient()
+
+    // watchEffect(()=>{
+    //     if(user.value){
+    //         return navigateTo("/")
+    //     }
+    // })
+
+    // const login=async (Prov)=>{
+    //     const {data,error}=await client.auth.signInWithAuth({
+    //         provider:prov,
+    //         redirectTo:window.location.origin
+    //     })
+
+    //     if (error) console.log(error)
+    // }
+
+</script>
