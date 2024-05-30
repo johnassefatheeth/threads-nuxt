03c5ce21aedb694065374b4adcedad4cc5e70412
John Assefa <johnassefaforbussines@gmail.com>
2024-05-30 02:13:25 -0700

    made the log out page

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,27 +1,25 @@
 <template>
-    <div id="AuthPAge" class="w-full h-[100vh] pt-32 bg-slate-500">
-        <div class="w-full justify-center items-center flex flex-col">
-            <div class="w-full flex items-center justify-center gap-2.5  p-2 bg-slate-600">
-                <img src="/public/threads-logo.png" alt="threads" class="w-[35px]">
-                <span class="font-bold text-2xl text-white">Threads</span>
-            </div>
-            <div class="max-w-[350px] mx-auto px-2 text-white ">
-                <div class="text-center mb-6 mt-4">Login / register</div>
-            </div>
-            <!-- <button class="flex items-center justify-center gap-3 p-1.5 w-full border rounded-full text-lg font-semibold ">
-                <div class="flex items-center gap-2 justify-center"> -->
-                    <!-- <img src="/public/github-logo.png" class="w-[30px] rounded-full"> -->
-                    <!-- Github
-                </div>
-            </button> -->
-
+    <div id="AuthPage" class="w-full h-screen flex flex-col items-center justify-center pt-32">
+      <div class="w-full justify-center items-center flex flex-col">
+        <div class="w-full flex items-center justify-center gap-2.5 p-2">
+          <img src="/public/threads-logo.png" alt="threads" class="w-[35px]">
+          <span class="font-bold text-2xl text-white">Threads</span>
         </div>
-
+        <div class="max-w-[350px] mx-auto px-2 text-white">
+          <div class="text-center mb-6 mt-4">Login / Register</div>
+        </div>
+        <button class="flex items-center justify-center border rounded-full text-lg font-semibold" style="width: 300px;">
+          <div class="flex items-center gap-2 justify-center text-white">
+            <img src="/public/github-logo.png" class="w-[35px] rounded-full">
+            Github
+          </div>
+        </button>
+      </div>
     </div>
-</template>
+  </template>
 
 <script setup>
-import { watchEffect } from 'vue';
+// import { watchEffect } from 'vue';
 
     // const user=useSupabaseUser()
     // const client=useSupabaseClient()
